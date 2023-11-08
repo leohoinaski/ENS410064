31294d982e8468b18edc935a8739253aa90ed9d8
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2023-11-08 16:57:29 -0300

    Update Aula05.py

--- scripts/Aula05.py
+++ scripts/Aula05.py
@@ -7,6 +7,8 @@ Created on Wed Nov  8 16:21:16 2023
 # Pacotes
 import netCDF4 as nc
 import matplotlib.pyplot as plt
+import numpy as np
+import geopandas as gpd
 
 # Caminho para meu arquivo netCDF com concentração de ozônio
 path = r"C:\Users\Leonardo.Hoinaski\Documents\ENS410064\dados\BRAIN_ClippedCONC_O3_2019_07_02_11_to_2019_12_30_23.nc"
@@ -20,8 +22,12 @@ lat = data['LAT'][:]
 lon = data['LON'][:]
 tflag = data['TFLAG'][:]
 
+# Figura de um ponto em todos os tempos
 fig,ax = plt.subplots()
 ax.plot(o3[:,0,50,50])
 
+# Figura de um tempo e todo o espaço
 fig2,ax2 = plt.subplots()
-ax2.pcolor(lon,lat,o3[0,0,:,:])
+ax2.pcolor(lon,lat,np.mean(o3[:,0,:,:],axis=0))
+shp = gpd.read_file(r"C:\Users\Leonardo.Hoinaski\Documents\ENS410064\dados\BR_Municipios_2022\BR_Municipios_2022.shp")
+shp.boundary.plot(ax=ax2)
